86f78d9d1071e0f34ac92b7ba1816557fcf95238
Terpe, Sven (SE-A/32) <sven.terpe@volkswagen.de>
2022-06-02 15:05:18 +0200

    accountController test

--- src/main/java/de/f73/account/application/api/AccountDTO.java
+++ src/main/java/de/f73/account/application/api/AccountDTO.java
@@ -1,2 +1,21 @@
-package de.f73.account.application.api;public class AccountDTO {
+package de.f73.account.application.api;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+public class AccountDTO {
+
+    private long accountNumber;
+    private String firstName;
+    private String lastName;
+
+    public AccountDTO(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+
+    }
+
+
 }
--- src/test/java/de/f73/account/application/api/AccountControllerTest.java
+++ src/test/java/de/f73/account/application/api/AccountControllerTest.java
@@ -1,2 +1,34 @@
-package de.f73.account.application.api;public class AccountControllerTest {
+package de.f73.account.application.api;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest
+public class AccountControllerTest {
+    @Autowired
+    MockMvc mvc;
+    ObjectMapper objectMapper = new ObjectMapper();
+    @Test
+    void createAccount_AccountDTO_Returns_AccountDTO() throws Exception {
+        //given
+        AccountDTO accountDTO = new AccountDTO("Alex", "Schulz");
+        //when
+        this.mvc.perform(MockMvcRequestBuilders.post("/account")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(this.objectMapper.writeValueAsString(accountDTO)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(objectMapper.writeValueAsString(accountDTO)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.accountNumber").value(123));
+    }
 }
